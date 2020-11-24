de9d6cfe64eda61311bb0d5002bbb38aa52e7e69
ESG <xmaoural@gmail.com>
2020-11-24 21:55:06 +0500

    add date filter

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -8,29 +8,29 @@
         >
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ date | date("datetime") }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
         <li>
           <a
             class="dropdown-trigger black-text"
-            href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
-          <ul id='dropdown' class='dropdown-content'>
+          <ul id="dropdown" class="dropdown-content">
             <li>
-              <a href="#" class="black-text">
+              <router-link :to="{ name: 'profile' }" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -42,9 +42,37 @@
 </template>
 
 <script>
+import M from "materialize-css/dist/js/materialize.min";
 export default {
   name: "Navbar",
-  emits: ["toggleSidebar"]
+  emits: ["toggleSidebar"],
+  data() {
+    return {
+      date: new Date(),
+      interval: null,
+      dropdown: null
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    });
+  },
+  methods: {
+    logout() {
+      console.log("logout");
+      this.$router.push({ name: "login", message: "logout" });
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  }
 };
 </script>
 
